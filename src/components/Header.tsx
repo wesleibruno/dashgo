38e97e2a214780847bbca3f09803a07cfaecde5f
wesleibruno <weslei945@gmail.com>
2022-01-31 17:54:41 -0300

    finalizando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine} from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export function Header() {
   return (
@@ -38,10 +42,35 @@ export function Header() {
           mx="4"
           mr="4"
           placeholder="Buscar na Plataforma"
-          _placeholder={{color: 'gray.400'}}
+          _placeholder={{ color: "gray.400" }}
         />
 
         <Icon as={RiSearchLine} />
+      </Flex>
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Weslei Bruno</Text>
+            <Text color="gray.300" fontSize="small">
+              weslei945@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Weslei Bruno" src="https://github.com/wesleibruno.png" />
+        </Flex>
       </Flex>
     </Flex>
   );
